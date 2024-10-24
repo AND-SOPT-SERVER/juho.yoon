02833c3ced23565793761be64890f98b26d57053
yoonjuho <dbswn990@naver.com>
2024-10-24 17:41:36 +0900

    feat: Diary Entity 구현

---
+++ src/main/java/org/sopt/diary/domain/Diary.java
@@ -0,0 +1,52 @@
+package org.sopt.diary.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.sopt.diary.config.domain.BaseEntity;
+
+@Entity
+public class Diary extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 100)
+    private String content;
+
+    protected Diary() {
+    }
+
+    public Diary(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Diary(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
