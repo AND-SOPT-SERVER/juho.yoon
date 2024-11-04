e408f2dbc2005b0a8792d27d34f6f2d349a3da32
yoonjuho <dbswn990@naver.com>
2024-11-04 20:15:53 +0900

    feat: 회원 식별 인터셉터 구현

---
+++ src/main/java/org/sopt/config/auth/LoginMemberArgumentResolver.java
@@ -0,0 +1,33 @@
+package org.sopt.config.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.sopt.member.service.MemberService;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final MemberService memberService;
+
+    public LoginMemberArgumentResolver(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        return memberService.findByMemberId(Long.parseLong(token));
+    }
+}
+
